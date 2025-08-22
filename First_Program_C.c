0d53ea5c82d708169265e0d10b54d3388326ca43
Sutton20-coder <msutton0353@floridapoly.edu>
2025-08-22 14:36:04 +0000

    Created the first Program

---
+++ First_Program_C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (void) {
+    printf("Hello\nWorld.\n");
+
+    printf("I\nam ready to code! \n");
+    return 0;
+}
+
+/* 
+Sample Comment.
+Multi-line.
+*/
+
+// Sample Comment. Single-line. 
+
+
