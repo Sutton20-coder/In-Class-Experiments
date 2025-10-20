8d4e96e22eb6b4b8036f1790f9693ff0901ff1b5
Sutton20-coder <msutton0353@floridapoly.edu>
2025-10-20 14:28:25 +0000

    Saved

---
+++ 25_Lesson.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "ctype.h"
+
+#define SIZE 100
+
+bool isUpper(char c);
+
+bool isLower(char c);
+
+bool isAlpha(char c); // True if the character is a letter upper or lower
+bool isDigit(char c); // True if c is a digit [0,9]
+bool isAlNum(char c); // True if the character is a letter or a digit
+
+char toLower(char c);
+
+char toUpper(char c);
+
+int main(void){
+
+    char ch = 'V';
+
+    //Create a boolean variable.
+
+    const bool isStudentHappyInCOP2271AfterWeek10 = true; 
+
+
+    ///////////////////////////////////////
+
+    char str[SIZE] = "Hello"; // {H, e, l, l, o, '\0'}
+
+    // Scan the string.
+
+    //scanf("%s", str); // Scans until the first whitespace.
+
+    fgets(str, SIZE - 1, stdin);
+
+    // Print the string.
+
+    puts("Printing with printf");
+
+    printf("%s\n\n", str); 
+
+    puts("Printing with a loop");
+
+    for(int i = 0; str[i] != '\0'; i++){
+        printf("%c", str[i]);
+    }
+    printf("\n\n");
+
+    // Print only lowercase letters.
+    
+    puts("Printing with lowercase letters");
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isLower(str[i])){
+            printf("%c\n", str[i]);
+        }
+    }
+
+
+
+    puts("Printing in reverse case");
+
+    for (int i = 0; str[i] != '\0'; i++){
+        if (isLower(str[i])){
+            printf("%c", toUpper(str[i]));
+        }
+        else{
+            printf("%c", toLower(str[i]));
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+
+bool isUpper(char c){
+    if(c >= 'A' && c <= 'Z'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool isLower(char c){
+    if(c >= 'a' && c <= 'z'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+bool isAlpha(char c){
+    return isUpper(c) || isLower(c);
+}
+
+bool isDigit(char c){
+    return (c >= '0' && c <= '9');
+}
+
+bool isAlNum(char c){
+    return isAlpha(c) || isDigit(c);
+}
+
+char toLower(char c){
+    if (isUpper(c)){
+        return c + 32; // Returns uppercase ascii code.
+    }
+    else{
+        return c;
+    }
+}
+
+char toUpper(char c){
+    if(isLower(c)){
+        return c - 32;
+    }
+    else{
+        return c;
+    }
+}
