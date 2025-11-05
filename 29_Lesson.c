acb29bde4da2d55d676815181fcde4de76f72a1a
Sutton20-coder <msutton0353@floridapoly.edu>
2025-11-05 15:52:25 +0000

    Saved2

--- 29_Lesson.c
+++ 29_Lesson.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
 
+#define MAX_WALLS 20
+
 typedef struct{
     double width;
     double height;
 } Rectangle;
 
+typedef struct{
+    Rectangle floor;
+    Rectangle walls[MAX_WALLS];
+    int count_walls;
+} Room;
+
 Rectangle Rectangle_create(double w, double h);
 
 double Rectangle_getPerimeter(Rectangle *r);
+
+Room Room_create(double w_floor, double h_floor);
+void Room_addWall(Room *r, double w_wall, double h_wall);
+
+double Room_getFloorPerimeter(Room *r);
+double Room_getWallsPerimeter(Room *r);
 
 int main (void){
     Rectangle rect = {15.4, 23.5};
@@ -32,6 +46,12 @@ int main (void){
     }
 
 
+    /*Room living_room = Room_create(12, 12);
+    Room_addWall(&living_room, 13, 13);
+    Room_addWall(&living_room, 13, 13);
+    Room_addWall(&living_room, 13, 13);
+    Room_addWall(&living_room, 13, 13);*/
+    
 
 
 
@@ -52,3 +72,22 @@ Rectangle Rectangle_create(double w, double h){
 double Rectangle_getPerimeter(Rectangle *r){
     return 2 * (r->width + r->height);
 }
+
+
+
+Room Room_create(double w_floor, double h_floor){
+    Room r;
+    r.floor = Rectangle_create(w_floor, h_floor);
+    r.count_walls = 0;
+    return r;
+}
+
+/*void Room_addWall(Room *r, double w_wall, double h_wall){
+    if(r->count_walls >= MAX_WALLS){
+        printf("Cannot have more than %d walls\n", MAX_WALLS);
+        return;
+    }
+    r->walls(r->count_walls) = Rectangle_create(w_wall, h_wall);
+    r->count_walls++;
+
+}*/
